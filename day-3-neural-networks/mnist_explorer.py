708817c6d8444162144eda9ff8040be250345b0c
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-09-03 09:24:15 +0200

    very important update

--- day-3-neural-networks/mnist_explorer.py
+++ day-3-neural-networks/mnist_explorer.py
@@ -2,10 +2,11 @@ import mnist_loader
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize_MNIST(sample, title = None, cmap = "Greys"):
+
+def visualize_MNIST(sample, title=None, cmap="Greys"):
     """
     A function for visualizing data samples from the MNIST database
-    
+
     Note that the greyscale is inversed, consequently the traditional black
     background is white.
     """
@@ -13,15 +14,16 @@ def visualize_MNIST(sample, title = None, cmap = "Greys"):
         img = np.array(np.split(sample, 28))[:, :, 0]
     else:
         img = np.array(np.split(sample, 28))
-    
+
     plt.matshow(img, cmap=cmap)
-    plt.axis('off')
+    plt.axis("off")
     if title:
         plt.title(title)
+
 
 if __name__ == "__main__":
     train_data, val_data, test_data = mnist_loader.load_data_wrapper()
     sample = train_data[0][0]
     solution = np.argmax(train_data[0][1])
     visualize_MNIST(sample, title=f"Solution: {solution} - Prediction: ?")
-    plt.savefig('img/sample.png')
+    plt.savefig("img/sample.png")
